01a7e45efe8c9fce05367101b052595a0321470b
Yufei Zhu <yufei.zhu@oru.se>
2024-06-02 22:10:23 +0200

    update

--- observe_and_update.py
+++ observe_and_update.py
@@ -31,16 +31,23 @@ def test_online_on_thor_magni():
     # thor_magni.plot_fov(robot_position, robot_facing_angle, fov_angle, fov_radius, observed_traj, f"cov")
     
     
-    #### For rectangular region ####
-    obs_x = 1
-    obs_y = 1
-    delta_x = 6
-    delta_y = 3
+    # #### For rectangular region ####
+    # obs_x = 1
+    # obs_y = 1
+    # delta_x = 6
+    # delta_y = 3
     
-    # obs_x = -7
-    # obs_y = -3.5
-    # delta_x = 5
-    # delta_y = 5
+    # # obs_x = -7
+    # # obs_y = -3.5
+    # # delta_x = 5
+    # # delta_y = 5
+    
+    
+    #### For ATC rectangular region ####
+    obs_x = 29
+    obs_y = -19
+    delta_x = 3
+    delta_y = 3
     
 
     thor_magni = InThorMagniDataset('config.yaml', raw_data_file='thor_magni_combine_add_time_all_dates_for_train_cliff_correct_ori/A.csv')
@@ -68,6 +75,59 @@ def test_online_on_thor_magni():
         
         # thor_magni.plot_region(obs_x, obs_y, delta_x, delta_y, observed_traj, f"{observe_start_time}_{observe_start_time + observe_period}")
         onlineUpdateMoD.updateMoD(observed_traj, f"B_{observe_start_time}_{observe_start_time + observe_period}")
+
+def test_online_on_atc():
+    
+    onlineUpdateMoD = OnlineUpdateMoD(
+        config_file="config.yaml",
+        current_cliff=None,
+        output_cliff_folder=f"online_mod_res_atc",
+        save_fig_folder=f"save_fig_online")
+    
+    ## parameters if use a cone view
+    # robot_position = np.array([0, 0])  # robot view location, 2d
+    # robot_facing_angle = 0  # robot facing direction (radians), 0 means facing right along the x-axis
+    # fov_angle = np.radians(120)  # Field of view angle in radians
+    # fov_radius = 2  # Field of view radius
+    
+    observe_period = 1000  # Observation period
+    observe_start_time = 0  # Observation start time
+
+    ##### For cone-shaped field of view #####
+    # observed_traj = thor_magni.get_observed_traj(robot_position, robot_facing_angle, fov_angle, fov_radius, observe_start_time, observe_period)    
+    # print(observed_traj)
+    # thor_magni.plot_fov(robot_position, robot_facing_angle, fov_angle, fov_radius, observed_traj, f"cov")
+    
+    
+    #### For ATC rectangular region ####
+    obs_x = 29
+    obs_y = -19
+    delta_x = 3
+    delta_y = 3
+
+    for hour in range(9, 21, 1):
+        thor_magni = InThorMagniDataset('config.yaml', raw_data_file=f'/home/yufei/research/parse_dataset/parse_atc/atc-1s-ds-1024-split-hour/atc-1024-{hour}.csv')
+        observed_traj = thor_magni.get_observed_traj_region_all_time(obs_x, obs_y, delta_x, delta_y)
+        # observed_traj = thor_magni.get_observed_traj_all_area(obs_x, obs_y, delta_x, delta_y, observe_start_time, observe_period)
+        thor_magni.plot_region_atc(obs_x, obs_y, delta_x, delta_y, observed_traj, f"{hour}_{hour + 1}")
+        onlineUpdateMoD.updateMoD(observed_traj, f"ATC1024_{hour}_{hour + 1}")
+
+
+def test_online_on_art():
+    
+    onlineUpdateMoD = OnlineUpdateMoD(
+        config_file="config.yaml",
+        current_cliff=None,
+        output_cliff_folder=f"online_mod_res_art",
+        save_fig_folder=f"save_fig_online")
+
+    for angle in [0, 90, 180, 270]:
+        print("-------------------- In angle -------------------", angle)
+        thor_magni = InThorMagniDataset('config.yaml', raw_data_file=f'/home/yufei/research/online_mod/online_mod/online_batch_data/artv2/b1_{angle}.csv')
+        observed_traj = thor_magni.get_observed_traj_all_area_all_time()
+        # thor_magni.plot_region_atc(obs_x, obs_y, delta_x, delta_y, observed_traj, f"{hour}_{hour + 1}")
+        onlineUpdateMoD.updateMoD(observed_traj, f"b1_{angle}")
+
 
 
 def observe_and_update(observe_start_time, observe_period, observe_region):
@@ -130,4 +190,6 @@ def observe_and_update(observe_start_time, observe_period, observe_region):
     
 
 
-test_online_on_thor_magni()
+# test_online_on_thor_magni()
+# test_online_on_atc()
+test_online_on_art()
---
+++ plot_res_figure.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import explore_utils as utils
+import matplotlib as mpl
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize, LinearSegmentedColormap
+from matplotlib.patches import Patch
+
+import pandas as pd
+import numpy as np
+
+import os
+
+def read_cliff_map_data(cliff_map_file):
+    try:
+        data = pd.read_csv(cliff_map_file, header=None)
+    except pd.errors.EmptyDataError:
+        print(f"Empty data in {cliff_map_file}")
+        return np.array([])
+    
+    data.columns = ["x", "y", "velocity", "motion_angle",
+                    "cov1", "cov2", "cov3", "cov4", "weight", "motion_ratio"]
+    
+    return data.to_numpy()
+
+def plot_art_exp(cliff_map_file, output_fig_name):
+    plt.figure(figsize=(10, 8), dpi=100)
+    
+    cliff_map_data = read_cliff_map_data(cliff_map_file)
+
+    (u, v) = utils.pol2cart(cliff_map_data[:, 2], cliff_map_data[:, 3])
+    weight = cliff_map_data[:, 8]
+
+    for i in range(len(cliff_map_data)):
+        print(cliff_map_data[i, 0], cliff_map_data[i, 1], u[i], v[i])
+        plt.quiver(cliff_map_data[i, 0], cliff_map_data[i, 1], u[i], v[i], color='b', alpha=weight[i], angles='xy', scale_units='xy', scale=1, width=0.03)
+
+    plt.scatter(cliff_map_data[0, 0], cliff_map_data[0, 1], c='black', s=200)
+
+    norm = Normalize(vmin=0, vmax=1)
+    # cmap = cm.Blues
+    colors = [(0, 0, 1, alpha) for alpha in np.linspace(0, 1, 256)]
+    blue_cmap = LinearSegmentedColormap.from_list("BlueAlphaCmap", colors, N=256)
+    sm = plt.cm.ScalarMappable(cmap=blue_cmap, norm=norm)
+    sm.set_array([])
+    cbar = plt.colorbar(sm, ax=plt.gca(), shrink = 0.8, fraction=0.05, pad=0.06)
+    cbar.set_label('Weight', fontsize=20)
+    cbar.ax.tick_params(labelsize=20)
+
+
+    plt.axis('off')
+    plt.axis('equal')
+    plt.xlim(-1, 3)
+    plt.ylim(-1, 3)
+    
+    
+    plt.savefig(output_fig_name, bbox_inches='tight')
+    # plt.show()
+
+def plot_art_data(art_map_file, output_fig_name):
+    plt.figure(figsize=(10, 8), dpi=100)
+
+    data = pd.read_csv(art_map_file, header=None)
+    data.columns = ["time", "person_id", "x", "y", "velocity", "motion_angle"]
+    data['motion_angle'] = np.mod(data['motion_angle'], 2 * np.pi)
+    data = data[['time', 'x', 'y', 'velocity', 'motion_angle']]
+
+    (u, v) = utils.pol2cart(data[['velocity']].values, data[['motion_angle']].values)
+    plt.quiver(data['x'].values, data['y'].values, u, v, color="black", alpha=0.6, angles='xy', scale_units='xy', scale=1, width=0.004)
+
+    plt.axis('off')
+    plt.axis('equal')
+    plt.xlim(-1, 3)
+    plt.ylim(-1, 3)
+    
+    plt.savefig(output_fig_name, bbox_inches='tight')
+    # plt.show()
+
+os.makedirs("online_mod_res_art/figs", exist_ok=True)
+# for type in ["all", "interval", "online"]:
+# # for type in ["all"]:
+#     for angle in [0, 90, 180, 270]:
+#         plot_art_exp(f"online_mod_res_art/b1_{angle}_{type}.csv", f"online_mod_res_art/figs/b1_{angle}_{type}.png")
+
+angle = 270
+type = "online"
+plot_art_exp(f"online_mod_res_art/b1_{angle}_{type}.csv", f"online_mod_res_art/figs/legend2.png")
+
+
+# for angle in [0, 90, 180, 270]:
+#     art_map_file = f'/home/yufei/research/online_mod/online_mod/online_batch_data/artv2/b1_{angle}.csv'
+#     plot_art_data(art_map_file, f"online_mod_res_art/figs/raw_data_{angle}.png")
